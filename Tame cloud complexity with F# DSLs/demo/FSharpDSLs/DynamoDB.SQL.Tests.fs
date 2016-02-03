eba077c0c979979070d2938648012e081e70ba75
Yan Cui <yan.cui@just-eat.com>
2016-02-03 01:41:25 +0000

    - added demo for F# DSL talk

---
+++ Tame cloud complexity with F# DSLs/demo/FSharpDSLs/DynamoDB.SQL.Tests.fs
@@ -0,0 +1,270 @@
+﻿module DynamoDB.SQL.Tests
+
+open FsUnit
+open NUnit.Framework
+open DynamoDB.SQL
+open FParsec
+
+let parseDynamoQuery str = 
+    match run pquery str with
+    | Success (x, _, _) -> x
+
+[<Test>]
+let ``when there is white spaces around the attribute names and table name they should be ignored`` () =
+    let select = "SELECT Name,    Age,
+                            Salary
+                    FROM   Employees 
+                    WHERE FirstName   =      \"Yan\"
+                    LIMIT     5"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Attribute "Name"; Attribute "Age"; Attribute "Salary" ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")) ]
+    query.Limit  |> should equal <| Some(Limit 5)
+
+[<Test>]
+let ``when the SELECT, FROM, WHERE and LIMIT keywords are not in capitals they should still be parsed correctly`` () =
+    let select = "sELeCT Name, Age, Salary
+                    FrOm Employees
+                    where FirstName = \"Yan\"
+                    liMIt 5"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Attribute "Name"; Attribute "Age"; Attribute "Salary" ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")) ]
+    query.Limit  |> should equal <| Some(Limit 5)
+
+[<Test>]
+let ``when there are multiple conditions in the where clause they should all be parsed`` () =
+    let select = "SELECT * FROM Employees 
+                  WHERE FirstName = \"Yan\" 
+                  AND LastName = \"Cui\""
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal(S "Yan")); (Attribute "LastName", Equal(S "Cui")) ]
+    query.Limit  |> should equal <| None
+
+[<Test>]
+let ``when < operator is used it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND Age < 99"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", LessThan (NBigInt 99I)) ]
+    query.Limit  |> should equal <| None
+
+[<Test>]
+let ``when <= operator is used it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND Age <= 99"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", LessThanOrEqual (NBigInt 99I)) ]
+    query.Limit  |> should equal <| None
+
+[<Test>]
+let ``when > operator is used it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND Age > 99"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", GreaterThan (NBigInt 99I)) ]
+    query.Limit  |> should equal <| None
+
+[<Test>]
+let ``when >= operator is used it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND Age >= 99"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", GreaterThanOrEqual (NBigInt 99I)) ]
+    query.Limit  |> should equal <| None
+
+[<Test>]
+let ``when the Begins With operator is used it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND LastName BEGINS WITH \"Cui\""
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "LastName", BeginsWith (S "Cui")) ]
+    query.Limit  |> should equal <| None
+    
+[<Test>]
+let ``when the Between operator is used it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND Age BETWEEN 10 AND 30"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", Between ((NBigInt 10I), (NBigInt 30I))) ]
+    query.Limit  |> should equal <| None
+
+[<Test>]
+let ``when limit clause is specified, it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND Age >= 30 LIMIT 10"
+
+    let query = parseDynamoQuery select
+            
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", GreaterThanOrEqual(NBigInt 30I)) ]
+    query.Limit  |> should equal <| Some(Limit 10)
+
+[<Test>]
+let ``when order asc is specified, it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND Age >= 30 ORDER ASC LIMIT 10"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", GreaterThanOrEqual(NBigInt 30I)) ]
+    query.Limit  |> should equal <| Some(Limit 10)
+    query.Order  |> should equal <| Some(Asc)
+
+[<Test>]
+let ``when order desc is specified, it should be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" AND Age >= 30 ORDER DESC LIMIT 10"
+
+    let query = parseDynamoQuery select
+
+    query.Action |> should equal <| Select [ Asterisk ]
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", GreaterThanOrEqual(NBigInt 30I)) ]
+    query.Limit  |> should equal <| Some(Limit 10)
+    query.Order  |> should equal <| Some(Desc)
+
+[<Test>]
+let ``when a count query is specified, it should be parsed correctly`` () =
+    let count = "COUNT * FROM Employees WHERE FirstName = \"Yan\" AND Age >= 30 ORDER DESC LIMIT 10"
+
+    let query = parseDynamoQuery count
+
+    query.Action |> should equal <| Count
+    query.From   |> should equal <| From "Employees"
+    query.Where  |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")); (Attribute "Age", GreaterThanOrEqual(NBigInt 30I)) ]
+    query.Limit  |> should equal <| Some(Limit 10)
+    query.Order  |> should equal <| Some(Desc)
+
+[<Test>]
+let ``when NoConsistentRead option is specified it should be captured in the Options clause`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" WITH (  nOConsiStentRead )"
+        
+    let query = parseDynamoQuery select
+
+    query.Action  |> should equal <| Select [ Asterisk ]
+    query.From    |> should equal <| From "Employees"
+    query.Where   |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")) ]
+    query.Options |> should equal <| Some [| NoConsistentRead |]
+
+[<Test>]
+let ``when PageSize option is specified it should be captured in the Options clause`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" WITH (Pagesize(  10) )"
+        
+    let query = parseDynamoQuery select
+
+    query.Action  |> should equal <| Select [ Asterisk ]
+    query.From    |> should equal <| From "Employees"
+    query.Where   |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")) ]
+    query.Options |> should equal <| Some [| QueryPageSize 10 |]
+
+[<Test>]
+let ``when Index option is specified with AllAttributes set to true it should be captured in the Options clause`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" WITH (Index( _M-y.1nd3x ,  true) )"
+        
+    let query = parseDynamoQuery select
+
+    query.Action  |> should equal <| Select [ Asterisk ]
+    query.From    |> should equal <| From "Employees"
+    query.Where   |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")) ]
+    query.Options |> should equal <| Some [| Index("_M-y.1nd3x", true) |]
+
+[<Test>]
+let ``when Index option is specified with AllAttributes set to false it should be captured in the Options clause`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" WITH (Index( _M-y.1nd3x ,  false) )"
+        
+    let query = parseDynamoQuery select
+
+    query.Action  |> should equal <| Select [ Asterisk ]
+    query.From    |> should equal <| From "Employees"
+    query.Where   |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")) ]
+    query.Options |> should equal <| Some [| Index("_M-y.1nd3x", false) |]
+
+[<Test>]
+let ``when NoReturnedCapacity option is specified it should be captured in the Options clause`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" WITH ( NoReturnedCapacity)"
+        
+    let query = parseDynamoQuery select
+
+    query.Action  |> should equal <| Select [ Asterisk ]
+    query.From    |> should equal <| From "Employees"
+    query.Where   |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")) ]
+    query.Options |> should equal <| Some [| QueryNoReturnedCapacity |]
+
+[<Test>]
+let ``when both NoConsistentRead and PageSize options are specified they should be captured in the Options clause`` () =
+    let select = "SELECT * FROM Employees WHERE FirstName = \"Yan\" WITH ( NOconsistentRead, Pagesize(  10) )"
+        
+    let query = parseDynamoQuery select
+
+    query.Action  |> should equal <| Select [ Asterisk ]
+    query.From    |> should equal <| From "Employees"
+    query.Where   |> should equal <| Where [ (Attribute "FirstName", Equal (S "Yan")) ]
+    query.Options |> should equal <| Some [| NoConsistentRead; QueryPageSize 10 |]
+            
+[<Test>]
+let ``when the attribute value is a very long number it should be parsed without losing precision`` () =
+    // from issue #29
+    let select = "SELECT * FROM pd_cs_friendshiprequest WHERE ForUserId = 10205471065416239"
+        
+    let query = parseDynamoQuery select
+    query.Where |> should equal <| Where [ (Attribute "ForUserId", Equal (NBigInt 10205471065416239I))]
+
+    // where the attribute value is > int64 but < uint64
+    let select = "SELECT * FROM pd_cs_friendshiprequest WHERE ForUserId = 9223372036854775809"
+
+    let query = parseDynamoQuery select
+    query.Where |> should equal <| Where [ (Attribute "ForUserId", Equal (NBigInt 9223372036854775809I))]
+
+    // where the attribute value is negative
+    let select = "SELECT * FROM pd_cs_friendshiprequest WHERE ForUserId = -9223372036854775808"
+
+    let query = parseDynamoQuery select
+    query.Where |> should equal <| Where [ (Attribute "ForUserId", Equal (NBigInt -9223372036854775808I))]
+
+[<Test>]
+let ``when the attribute value is a small number it should still be parsed correctly`` () =
+    let select = "SELECT * FROM Employees WHERE Id = 3"
+
+    let query = parseDynamoQuery select
+    query.Where |> should equal <| Where [ (Attribute "Id", Equal (NBigInt 3I))]
+
+    let select = "SELECT * FROM Employees WHERE Id = -3"
+
+    let query = parseDynamoQuery select
+    query.Where |> should equal <| Where [ (Attribute "Id", Equal (NBigInt -3I))]
+
+[<Test>]
+let ``when the attribute value is a decimal it should be parsed as NDouble`` () =
+    let select = "SELECT * FROM Employees WHERE Id = 42.32"
+
+    let query = parseDynamoQuery select
+    query.Where |> should equal <| Where [ (Attribute "Id", Equal (NDouble 42.32))]
---
+++ Tame cloud complexity with F# DSLs/demo/FSharpDSLs/DynamoDB.SQL.fs
@@ -0,0 +1,273 @@
+﻿module DynamoDB.SQL
+
+open System
+open FParsec
+open FParsec.Internals
+
+type Identifier = 
+    | Asterisk
+    | Attribute of string
+
+type Operant = 
+    | S         of string
+    | NDouble   of double
+    | NBigInt   of bigint
+
+type FilterCondition = 
+    | Equal                 of Operant
+    | NotEqual              of Operant
+    | GreaterThan           of Operant
+    | GreaterThanOrEqual    of Operant
+    | LessThan              of Operant
+    | LessThanOrEqual       of Operant
+    | NotNull
+    | Null
+    | Contains              of Operant
+    | NotContains           of Operant
+    | BeginsWith            of Operant    
+    | Between               of Operant * Operant
+    | In                    of Operant list
+
+type OrderDirection =
+    | Asc
+    | Desc
+
+type Action = 
+    | Select of Identifier list
+    | Count
+
+type From   = From of string
+type Filter = Identifier * FilterCondition
+type Where  = Where of Filter list
+type Limit  = Limit of int
+
+type QueryOption =
+    | NoConsistentRead
+    | QueryPageSize of int
+    | Index         of index:string * allAttributes:bool
+    | QueryNoReturnedCapacity
+
+type DynamoQuery =
+    {
+        Action  : Action
+        From    : From
+        Where   : Where
+        Limit   : Limit option
+        Order   : OrderDirection option
+        Options : QueryOption [] option
+    }
+
+
+let (<&&>) f g x = (f x) && (g x)
+let (<||>) f g x = (f x) || (g x)
+
+
+type Parser<'t> = Parser<'t, unit>
+
+let ws = spaces // eats any whitespace
+
+// helper functions that ignores subsequent whitespaces
+let pstring s        : Parser<_> = pstring s .>> ws
+let skipString s     : Parser<_> = skipStringCI s .>> ws
+let stringReturn s r : Parser<_> = stringCIReturn s r .>> ws
+let pfloat  : Parser<float> = pfloat .>> ws
+let pint32  : Parser<int32> = pint32 .>> ws
+
+
+
+let pasterisk = stringReturn "*" Asterisk
+
+let isAttrName = isLetter <||> isDigit
+let pattribute = 
+    many1SatisfyL isAttrName "attribute name"
+    .>> ws 
+    |>> Attribute
+
+let comma = pstring ","
+
+let pattributes = choice [ pasterisk; pattribute ]
+let pselect = 
+    skipString "select" 
+    >>. (sepBy1 pattributes comma |>> Select)
+
+let pcount = stringReturn "count *" Count
+
+// an action is either select or count
+let paction = ws >>. choice [ pselect; pcount ] .>> ws
+
+let isTableName = isLetter <||> isDigit <||> isAnyOf ['.'; '-'; '_']
+let pfrom : Parser<_> =
+    ws
+    >>. skipString "from"
+    >>. ((many1SatisfyL isTableName "table name") |>> From)
+    .>> ws
+
+let v1 = pipe2 paction pfrom (fun act table -> act, table)
+
+
+
+let (|BigInt|_|) x =
+    match bigint.TryParse x with
+    | true, x -> Some x
+    | _ -> None
+
+// can't just use pfloat here because it loses precision over very
+// large numbers e.g. 1020547106541605689
+let pnumber : Parser<_> = 
+    many1Satisfy2 (isDigit <||> ((=) '-')) (isDigit <||> ((=) '.'))
+    |>> (function 
+        | BigInt x -> NBigInt x
+        | x -> Double.Parse x |> NDouble)
+        
+let pstringLiteral =
+    between (pstring "\"") (pstring "\"") (manySatisfy isAsciiLetter)
+    |>> S
+
+let operant = 
+    ws 
+    >>. choiceL [ pstringLiteral; pnumber ] "String or Numeric value" 
+    .>> ws
+
+
+let binaryOperators = 
+    choice [ 
+        stringReturn "="  Equal
+        stringReturn ">=" GreaterThanOrEqual
+        stringReturn ">"  GreaterThan
+        stringReturn "<=" LessThanOrEqual
+        stringReturn "<"  LessThan
+        stringReturn "begins with" BeginsWith ]
+let binaryCondition = 
+    pipe3 
+        pattribute binaryOperators operant 
+        (fun id op v -> id, op v)
+
+let between = stringReturn "between" Between
+let and'    = skipString "and"
+let betweenCondition = 
+    pipe5
+        pattribute between operant and' operant 
+        (fun id op v1 _ v2 -> id, op(v1, v2))
+
+let pcondition = 
+    choice [ 
+        attempt binaryCondition; 
+        attempt betweenCondition ]
+let filterConditions = sepBy1 (ws >>. pcondition .>> ws) (ws >>. and')
+let pwhere = 
+    ws 
+    >>. skipString "where" 
+    >>. (filterConditions |>> Where) 
+    .>> ws
+
+let v2 = 
+    pipe3 
+        paction pfrom pwhere 
+        (fun act from where -> 
+            {
+                Action  = act
+                From    = from
+                Where   = where
+                Order   = None
+                Limit   = None
+                Options = None
+            })
+
+
+
+let plimit = ws >>. skipString "limit" >>. pint32 |>> Limit
+
+let porder = 
+    ws 
+    >>. choice [ 
+            stringReturn "order asc" Asc
+            stringReturn "order desc" Desc ]
+    .>> ws
+
+let v3 = 
+    pipe5 
+        paction pfrom pwhere (opt porder) (opt plimit)
+        (fun act from where order limit -> 
+            {
+                Action  = act
+                From    = from
+                Where   = where
+                Order   = order
+                Limit   = limit
+                Options = None
+            })
+
+
+
+
+let noconsistentRead = stringReturn "noconsistentread" NoConsistentRead
+
+let openParen  = skipString "("
+let closeParen = skipString ")"
+
+// PageSize (42)
+let queryPageSize  = 
+    skipString "pagesize" 
+    >>. openParen 
+    >>. pint32 
+    .>> closeParen 
+    |>> QueryPageSize
+
+// DynamoDB allows a-z, A-Z, 0-9, _, - and . in the index name
+let isValidChar c = 
+    isAsciiLetter c || isDigit c || c = '_' || c = '-' || c = '.'
+let pindexName = 
+    ws 
+    >>. identifier (IdentifierOptions(isAsciiIdStart = isValidChar, isAsciiIdContinue = isValidChar)) 
+    .>> ws
+    
+let pTrue, pFalse = stringReturn "true" true, stringReturn "false" false
+
+// index (MyIndex, true)
+let index = 
+    skipString "index" 
+    >>. openParen
+    >>. (tuple3 pindexName comma (choice [ pTrue; pFalse ])
+        |>> (fun (idxName, _, isAll) -> idxName, isAll))
+    .>> closeParen
+    |>> Index
+
+let noReturnedCapacity = stringReturn "NoReturnedCapacity" QueryNoReturnedCapacity
+
+let queryOption  = 
+    choice [ 
+        noconsistentRead
+        queryPageSize
+        index
+        noReturnedCapacity ]
+let queryOptions = sepBy1 queryOption comma |>> List.toArray
+
+// with (noreturnedcapacity, noconsistentread, pagesize (42))
+let pwith = 
+    ws 
+    >>. skipString "with" 
+    >>. openParen 
+    >>. queryOptions 
+    .>> closeParen
+
+
+
+
+// FParsec only supports up to pipe5, extend it by piping the result of the first 5 parser through a second pipe
+let pipe6 (p1: Parser<'a,'u>) (p2: Parser<'b,'u>) (p3: Parser<'c,'u>) (p4: Parser<'d,'u>) (p5: Parser<'e,'u>) (p6: Parser<'f, 'u>) g =
+    pipe2 (pipe5 p1 p2 p3 p4 p5 (fun a b c d e -> a, b, c, d, e)) p6 (fun (a, b, c, d, e) f -> g a b c d e f)
+
+let tuple6 p1 p2 p3 p4 p5 p6 = pipe6 p1 p2 p3 p4 p5 p6 (fun a b c d e f -> (a, b, c, d, e, f))
+
+
+let pquery : Parser<DynamoQuery> = 
+    tuple6 paction pfrom pwhere (opt porder) (opt plimit) (opt pwith)
+    |>> (fun (action, from, where, order, limit, with') -> 
+        { 
+            Action = action
+            From = from
+            Where = where
+            Limit = limit
+            Order = order
+            Options = with' 
+        })
